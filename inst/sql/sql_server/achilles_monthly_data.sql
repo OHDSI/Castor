6e722430dc53ac2855e0a36707fbebd982e9cb93
amolin19 <amolin19@its.jnj.com>
2020-12-11 11:44:47 -0500

    replaced substring() with left() to address issue #2

--- inst/sql/sql_server/achilles_monthly_data.sql
+++ inst/sql/sql_server/achilles_monthly_data.sql
@@ -14,7 +14,7 @@ select ar.stratum_2,
 select concat(num.stratum_2,'01') start_date,
        num.count_value,
        1000*num.count_value/denom.count_value as prevalence,
-	   num.count_value/sum(num.count_value)over(partition by substring(num.stratum_2,1,4)) proportion_within_year
+	   num.count_value/sum(num.count_value)over(partition by left(num.stratum_2,4)) proportion_within_year
   from num
   join @results_schema.achilles_results denom
     on num.stratum_2 = denom.stratum_1 and denom.analysis_id = 117
