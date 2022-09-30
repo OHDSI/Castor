d0e5691914d66322a5af985e31196a060fdeec00
AnthonyMolinaro <amolin19@its.jnj.com>
2022-09-30 18:51:39 -0400

    Updated seasonality score after discussion with Frank, need to update further for rare event edge cases

--- R/getSeasonalityScore.r
+++ R/getSeasonalityScore.r
@@ -7,12 +7,11 @@
 #'@details
 #' The degree of seasonality of a monthly time series is based on its departure from a uniform distribution.
 #' If the number of cases for a given concept is uniformly distributed across all time periods (in this case, all months), 
-#' then its monthly prevalence (as well as proportion) would be constant.  In this case, the time series would be
-#' considered "absolutely non-seasonal" and its "seasonality score" would be zero.
-#' Similarly, if all the cases occur at a single point in time (that is, in a single month), such a time series would be considered
-#' "absolutely seasonal" and its seasonality score would be 1.  All other time series would have
+#' then its monthly proportion would be approximately constant.  In this case, the time series would be
+#' considered "strictly non-seasonal" and its "seasonality score" would be zero.
+#' Similarly, if all cases recur at a single point in time (that is, in a single month), such a time series would be considered
+#' "strictly seasonal" and its seasonality score would be 1.  All other time series would have
 #' a seasonality score between 0 and 1.  Currently, only monthly time series are supported.
-#' NB: To be able to compute a seasonality score, a time series must have a minimum of three complete years of data. 
 #'    
 #'@param tsData  A time series object.
 #'
@@ -23,22 +22,17 @@
 
 getSeasonalityScore <- function(tsData)
 {
-	tsObj <- tsData
-	maxDist   <- 1.83
 	unifDist  <- 1/12
-	minMonths <- 36
-
-	if (frequency(tsObj) != 12)
-		stop("ERROR: Only monthly time series are supported")
-		
-	tsObj <- Castor::tsCompleteYears(tsObj)
-
-	if (length(tsObj) < minMonths)
-		stop("ERROR: Time series must have a minimum of three complete years of data")
+	a         <- c(1,rep(0,11))
+	maxDist   <- sum(abs(a-unifDist))
 	
-	tsObj.yrProp <- Castor::sumAcrossYears(tsObj)$PROP
+	tsObj <- tsData
+	tsObj <- Castor::tsCompleteYears(tsObj)
+	
+	M <- matrix(data=tsObj, ncol=12, byrow=TRUE)
+	ss <- sum(abs(t((rep(1,dim(M)[1]) %*% M)/as.integer(rep(1,dim(M)[1]) %*% M %*% rep(1,12))) - unifDist))/maxDist
 
-	tsObj.ss <- round(sum(abs(tsObj.yrProp-unifDist))/maxDist,2)
+	tsObj.ss <- round(ss,2)
 
 	return (tsObj.ss)
 }
